afb6908428a3dc9f4c3e1455f6d6091ac2dc5a4f
Hariom Thadke <hariomthadke@gmail.com>
2024-08-14 15:55:35 +0530

    second commit

--- BookMyFlight/src/main/java/com/nt/rest/MainRestController.java
+++ BookMyFlight/src/main/java/com/nt/rest/MainRestController.java
@@ -16,11 +16,16 @@ import com.nt.model.UserDetails;
 import com.nt.service.IUserDetailsService;
 
 @RestController("RestController")
-@RequestMapping("/save")
+@RequestMapping
 public class MainRestController {
 
 	@Autowired
 	private IUserDetailsService userDetailsService;
+	
+	@GetMapping("/")
+	public ResponseEntity<String> returnSuccessMessage(){
+		return new ResponseEntity<String>("Success",HttpStatus.OK);
+	}
 
 	@PostMapping("/registerUser")
 	public ResponseEntity<UserDetails> saveUserDetails(@RequestBody UserDetails userDetails){
